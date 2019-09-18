a8ca54c2cd1fdec7c2e1bf2f3ec45f258051e027
AkhilaErrabelly <aerrabelly@gmail.com>
2019-09-18 12:56:27 -0400

    Inital Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2l0eW9mZGV0cm9pdCIsImEiOiJjajd3MGlodXIwZ3piMnhudmlzazVnNm44In0.BL29_7QRvcnOrVuXX_hD9A';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    zoom: 10.7,
+    center: [-83.060303, 42.348495]
+});
