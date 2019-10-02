dab9a0d6de61a35750c56a2862f7c4230f1457e1
AkhilaErrabelly <aerrabelly@gmail.com>
2019-10-02 17:20:52 -0400

    changed JSON to geoJson

--- components/yelp.js
+++ components/yelp.js
@@ -1,6 +1,5 @@
 exports.yelp = function(){
     const yelp = require('yelp-fusion');
-
 // Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
     const apiKey = 'MIzyyL2O3CO86GTDo9ISK3LZWujAXdzDWKluJx96oAAJtEQg05zxVlM74TSnMi472hZJJkTp40DF8a59ALGftK8wUnJbobbW4eTNBQZu2O8fwo2-x966_plTV3SCXXYx';
@@ -9,7 +8,6 @@ exports.yelp = function(){
         term:'Free Wifi',
         location: 'detroit, mi'
     };
-
     const client = yelp.client(apiKey);
 
     client.search(searchRequest).then(response => {
--- index.js
+++ index.js
@@ -7,10 +7,80 @@ var map = new mapboxgl.Map({
     zoom: 10.7,
     center: [-83.060303, 42.348495]
 });
+//============================================================================
+var size = 100;
+
+var pulsingDot = {
+    width: size,
+    height: size,
+    data: new Uint8Array(size * size * 4),
+
+    onAdd: function() {
+        var canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.context = canvas.getContext('2d');
+    },
+
+    render: function() {
+        var duration = 1000;
+        var t = (performance.now() % duration) / duration;
+
+        var radius = size / 2 * 0.3;
+        var outerRadius = size / 2 * 0.7 * t + radius;
+        var context = this.context;
+
+// draw outer circle
+        context.clearRect(0, 0, this.width, this.height);
+        context.beginPath();
+        context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
+        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+        context.fill();
+
+// draw inner circle
+        context.beginPath();
+        context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+        context.fillStyle = 'rgba(255, 100, 100, 1)';
+        context.strokeStyle = 'white';
+        context.lineWidth = 2 + 4 * (1 - t);
+        context.fill();
+        context.stroke();
+
+// update this image's data with data from the canvas
+        this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+// keep the map repainting
+        map.triggerRepaint();
+
+// return `true` to let the map know that the image was updated
+        return true;
+    }
+};
+//==========================================================
+map.on('load', function(){
+    map.addSource('point',{
+        "type": 'geojson',
+        "data": {
+            "type": "FeatureCollection",
+            "features": geoJSON
+        }
+    });
+    map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+
+    map.addLayer({
+        "id": "points",
+        "type": "symbol",
+        "source": 'wifi-location',
+        "layout": {
+            "icon-image": "pulsing-dot"
+        }
+    });
+
+    })
+
 //================ geocoder for address search====================//
 let geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
-
     // limit results to North America
     countries: 'us',
     // further limit results to the geographic bounds representing the region of
@@ -29,60 +99,53 @@ let geocoder = new MapboxGeocoder({
     },
     mapboxgl: mapboxgl
 });
+
+function updateMap(data){
+    console.log(data.length);
+    var geoJSON ={
+
+
+    }
+}
 document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
-
-
 //================ geocoder for address search ends====================//
 //=================yelp for wifi search ============================//
-
-var url = 'https://api.yelp.com/v3/businesses/search?text=coffee&latitude=37.786882&longitude=-122.399972';
+const url = 'https://apis.detroitmi.gov/crowdsource/yahoo/wifi/locations/';
 fetch(url, {
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-        'Authorization': 'Bearer MIzyyL2O3CO86GTDo9ISK3LZWujAXdzDWKluJx96oAAJtEQg05zxVlM74TSnMi472hZJJkTp40DF8a59ALGftK8wUnJbobbW4eTNBQZu2O8fwo2-x966_plTV3SCXXYx'
-    },
     redirect: 'follow', // manual, *follow, error
-    referrer: 'no-referrer' // no-referrer, *client
-
 })
     .then(resp => resp.json())
-    console.log(resp)
     // Transform the data into json
     .then((data) => {
-        console.log(data);
 
-    }) // then data
-// var data = null;
-// var xhr = new XMLHttpRequest();
-// xhr.open("GET", "https://api.yelp.com/v3/businesses/search?text=coffee&latitude=37.786882&longitude=-122.399972");
-// xhr.setRequestHeader("Authorization", "Bearer MIzyyL2O3CO86GTDo9ISK3LZWujAXdzDWKluJx96oAAJtEQg05zxVlM74TSnMi472hZJJkTp40DF8a59ALGftK8wUnJbobbW4eTNBQZu2O8fwo2-x966_plTV3SCXXYx");
-// xhr.addEventListener("readystatechange", function () {
-//     if (this.readyState === 4) {
-//         data = JSON.parse(this.responseText);
-//         console.log(data);
-//     }
-// });
-// xhr.send(data);
-// const yelp = require('yelp-fusion');
-//
-// // Place holder for Yelp Fusion's API Key. Grab them
-// // from https://www.yelp.com/developers/v3/manage_app
-// const apiKey = 'MIzyyL2O3CO86GTDo9ISK3LZWujAXdzDWKluJx96oAAJtEQg05zxVlM74TSnMi472hZJJkTp40DF8a59ALGftK8wUnJbobbW4eTNBQZu2O8fwo2-x966_plTV3SCXXYx';
-//
-// const searchRequest = {
-//     term:'Four Barrel Coffee',
-//     location: 'san francisco, ca'
-// };
-//
-// const client = yelp.client(apiKey);
-//
-// client.search(searchRequest).then(response => {
-//     const firstResult = response.jsonBody.businesses[0];
-//     const prettyJson = JSON.stringify(firstResult, null, 4);
-//     console.log(prettyJson);
-// }).catch(e => {
-//     console.log(e);
-// });
+        var geoJSON = {
+            type: "FeatureCollection",
+            features: [],
+        };
+        for (let i = 0; i < data.length; i++) {
+            geoJSON.features.push({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [data[i].coordinates.longitude, data[i].coordinates.latitude]
+                },
+                "properties": {
+                    "id": data[i].id,
+                    "stationName": data[i].alias,
+                    "isClosed": data[i].is_closed,
+                    "station": data[i].stationName,
+                    "stAddress1": data[i].location.address1,
+                    "stAddress2": data[i].stAddress2,
+                    "city": data[i].location.city,
+                    "postalCode": data[i].location.zip_code,
+                }
+            });
+        } // then data
+
+        console.log(data[0].is_closed);
+
+         document.getElementById('geojson').innerHTML = JSON.stringify(geoJSON, null, 2);
+    })
