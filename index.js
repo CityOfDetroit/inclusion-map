e5448a31e9d380f548d6bbb0d7460afae372d913
AkhilaErrabelly <aerrabelly@gmail.com>
2019-11-21 12:30:57 -0500

    dist refresh

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let geocoder = new MapboxGeocoder({
         },
     // further limit results to the geographic bounds representing the region of
     // Detroit Michigan
-     //bbox: [-83.3437, 42.2102, -82.8754, 42.5197],
+     bbox: [-83.3437, 42.2102, -82.8754, 42.5197],
     // apply a client side filter to further limit results to those strictly within
     // the detroit michigan region
     filter: function(item) {
@@ -61,52 +61,54 @@ map.on('load', function(){
             "circle-radius": 10,
             "circle-color": "#007cbf"
         }
+
     });
-//     const url =  baseUrl + '42.3314,-83.0458';
-//     fetch(url, {
-//         method: 'GET', // *GET, POST, PUT, DELETE, etc.
-//         mode: 'cors', // no-cors, *cors, same-origin
-//         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//         redirect: 'follow', // manual, *follow, error
-//     }) .then(resp => resp.json())
-//     // Transform the data into json
-//     .then((data) => {
-//         console.log(data)
-//         for (let i = 0; i < data.length; i++) {
-//             var popup = new mapboxgl.Popup()
-//                 .setHTML(
-//                     '<div class="card mb-3">' +
-//                     '  <div class="row no-gutters">' +
-//                     '    <div class="col-md-4">' +
-//                     '<img class="card-img" src="' + data[i].image_url + '"/>' +
-//                     '    </div>' +
-//                     '    <div class="col-md-8">' +
-//                     '      <div class="card-body">' +
-//                     '        <h6 class="card-title">'+data[i].name+'</h6>' +
-//                     '<p class="place_address">'
-//                     +'<a href="">'
-//                     +data[i].location.address1 +','+ data[i].location.city+','
-//                     + data[i].location.state+',' + data[i].location.zip_code +
-//                     '</a>'+
-//                     '</p>' +
-//                     '<div class="rating" data-rating="'+ data[i].rating+'"><div class="star"></div> <div class="star"></div> <div class="star"></div> <div class="star"></div> <div class="star"></div> </div>'+
-//                     '      </div>' +
-//                     '    </div>' +
-//                     '  </div>' +
-//                     '</div>'
-//                 );
-//             //console.log(data[i].rating);
-//                 // .setText(data[i].name);
-// // create DOM element for the marker
-//             var el = document.createElement('div');
-//             el.id = 'marker';
-// // create the marker
-//             var marker = new mapboxgl.Marker()
-//                 .setLngLat([data[i].coordinates.longitude, data[i].coordinates.latitude])
-//                 .setPopup(popup)
-//                 .addTo(map);
-//     };
-// });
+
+    const url =  baseUrl + '42.3314,-83.0458';
+    fetch(url, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        redirect: 'follow', // manual, *follow, error
+    }) .then(resp => resp.json())
+    // Transform the data into json
+    .then((data) => {
+        console.log(data)
+        for (let i = 0; i < data.length; i++) {
+            var popup = new mapboxgl.Popup()
+                .setHTML(
+                    '<div class="card mb-3">' +
+                    '  <div class="row no-gutters">' +
+                    '    <div class="col-md-4">' +
+                    '<img class="card-img" src="' + data[i].image_url + '"/>' +
+                    '    </div>' +
+                    '    <div class="col-md-8">' +
+                    '      <div class="card-body">' +
+                    '        <h6 class="card-title">'+data[i].name+'</h6>' +
+                    '<p class="place_address">'
+                    +'<a href="">'
+                    +data[i].location.address1 +','+ data[i].location.city+','
+                    + data[i].location.state+',' + data[i].location.zip_code +
+                    '</a>'+
+                    '</p>' +
+                    '<div class="rating" data-rating="'+ data[i].rating+'"><div class="star"></div> <div class="star"></div> <div class="star"></div> <div class="star"></div> <div class="star"></div> </div>'+
+                    '      </div>' +
+                    '    </div>' +
+                    '  </div>' +
+                    '</div>'
+                );
+            //console.log(data[i].rating);
+                // .setText(data[i].name);
+// create DOM element for the marker
+            var el = document.createElement('div');
+            el.id = 'marker';
+// create the marker
+            var marker = new mapboxgl.Marker()
+                .setLngLat([data[i].coordinates.longitude, data[i].coordinates.latitude])
+                .setPopup(popup)
+                .addTo(map);
+    };
+});
      getUserLocation()
      getGeocoderResults()
     
@@ -115,9 +117,19 @@ let getGeoJson ={
     type: "FeatureCollection",
     features: [],
 }
+const checkStatus = response =>{
+    if(response.ok){
+        return response;
+    } else{
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+}
 function getGeocoderResults(){
+
     geocoder.on('result', function(ev) {
-        map.setZoom(14);
+        map.setZoom(13);
         map.getSource('places').setData(ev.result.geometry);
        
         console.log('ev',ev)
@@ -131,15 +143,13 @@ function getGeocoderResults(){
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             redirect: 'follow', // manual, *follow, error
         })
+            .then(checkStatus)
             .then(resp => resp.json())
             // Transform the data into json
             .then((data) => {
                 console.log(data)
                 const geoJson = getGeoJson;
-    
                 for (let i = 0; i < data.length; i++) {
-                    
-    
                     getGeoJson.features.push({
                         "type": "Feature",
                         "geometry": {
@@ -161,6 +171,14 @@ function getGeocoderResults(){
                         }
                     });
                     console.log(data[i]);
+                    // create DOM element for the marker
+                    var el = document.createElement('div');
+                    el.id = 'marker';
+// create the marker
+                    var marker = new mapboxgl.Marker()
+                        .setLngLat([data[i].coordinates.longitude, data[i].coordinates.latitude])
+                        .setPopup(popup)
+                        .addTo(map);
                     // document.getElementById('geojson').innerHTML = JSON.stringify(geoJSON, null, 2);
                     var popup = new mapboxgl.Popup()
                         .setHTML(
@@ -184,15 +202,7 @@ function getGeocoderResults(){
                             '  </div>' +
                             '</div>'
                         );
-                    
-    // create DOM element for the marker
-                    var el = document.createElement('div');
-                    el.id = 'marker';
-    // create the marker
-                    var marker = new mapboxgl.Marker()
-                        .setLngLat([data[i].coordinates.longitude, data[i].coordinates.latitude])
-                        .setPopup(popup)
-                        .addTo(map);
+
                 }
             })
 
@@ -227,6 +237,4 @@ function getUserLocation() {
         // if device doesnt support location
         console.log('device doesnt support location')
     }
-
 }; /* END getUserLocation(); */
-
